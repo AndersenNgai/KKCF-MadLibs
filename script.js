f16155b4efcffca4f918b816f13c34b8fc47fd7d
Andersen <97132038+AndersenNgai@users.noreply.github.com>
2024-07-31 18:06:48 +0000

    Final

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 // Once upon a time, a [giant] air-breathing [Kraken] that came from the ocean [began] to attack a city.
-// A tiny boy ate 10 radioactive spiders and 5 radioactive (user input), so he ate 5 + 10 = 15 things.
-// He turned into Superman or something, then attacked the Kraken.
+// A tiny boy ate 10 radioactive spiders and 5 [user input], so he ate 5 + 10 = 15 things.
+// He turned into Spiderman or something, then attacked the Kraken.
 // The prophecy that the boy would defeat the Kraken was [false].
-// The Kraken slammed the boy into a building, crushed him, and destroyed the city.
+// The Kraken flung the boy 10,000 miles away, crushed him, and destroyed the city.
+let noun = "Kraken";
+let adj = "giant";
+let verb = "began";
+let number = 10;
+let eaten = number + 5;
+let bool = false;
+let input = prompt("Enter a plural noun, preferably something that can be eaten...");
+console.log("Once upon a time, a " + adj + " air-breathing " + noun + " that came up from the ocean " +
+   verb + " to attack a city. A tiny boy ate " + number + " radioactive spiders and 5 " + input +
+    ", so he ate " + eaten + " things. The prophecy that the boy would defeat the Kraken was " +
+   bool + ". The Kraken flung the boy 10,000 miles away and destroyed the city.");
