6340ec496fa1e36b5c8d54260de66f7f176efce4
Andersen <97132038+AndersenNgai@users.noreply.github.com>
2024-07-31 17:42:54 +0000

    Plotline

---
+++ script.js
@@ -0,0 +1,5 @@
+// Once upon a time, a [giant] air-breathing [Kraken] that came from the ocean [began] to attack a city.
+// A tiny boy ate 10 radioactive spiders and 5 radioactive (user input), so he ate 5 + 10 = 15 things.
+// He turned into Superman or something, then attacked the Kraken.
+// The prophecy that the boy would defeat the Kraken was [false].
+// The Kraken slammed the boy into a building, crushed him, and destroyed the city.
